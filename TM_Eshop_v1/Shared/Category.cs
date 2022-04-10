50a90f0642d70ac1642f6bf97dbe2f21e352b6bf
Marek Tyl <tylmarek@outlook.com>
2022-04-10 12:51:45 +0200

    adding product and category

---
+++ TM_Eshop_v1/Shared/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM_Eshop_v1.Shared
+{
+    public class Category
+    {
+        public int CatId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string CatUrl { get; set; } = string.Empty;
+    }
+}
---
+++ TM_Eshop_v1/Shared/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM_Eshop_v1.Shared
+{
+    public class Product
+    {
+        public int ProId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string ProducerName { get; set; } = string.Empty;
+        public string EANCode { get; set; } = string.Empty;
+        public string DescriptionShort { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Category? Kategorie { get; set; }
+        public int KategorieId { get; set; }
+    }
+}
