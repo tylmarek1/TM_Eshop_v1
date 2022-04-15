ccc470bfeb5c5cd0777df4324caf10392983570f
Marek Tyl <tylmarek@outlook.com>
2022-04-15 10:26:49 +0200

    working add to cart button

---
+++ TM_Eshop_v1/Client/Services/CartService/ICartService.cs
@@ -0,0 +1,9 @@
+﻿namespace TM_Eshop_v1.Client.Services.CartService
+{
+    public interface ICartService
+    {
+        event Action OnChange;
+        Task AddToCart(CartItem cartItem);
+        Task<List<CartItem>> GetCartItems();
+    }
+}
