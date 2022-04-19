43c63b052634a8a55593219c9271615321e6ccb7
Marek Tyl <tylmarek@outlook.com>
2022-04-19 19:00:53 +0200

    kosik ok

--- TM_Eshop_v1/Client/Services/OrderService/OrderService.cs
+++ TM_Eshop_v1/Client/Services/OrderService/OrderService.cs
@@ -6,11 +6,13 @@ namespace TM_Eshop_v1.Client.Services.OrderService
     {
         private readonly HttpClient _http;
         private readonly NavigationManager _navigationManager;
+        private readonly ILocalStorageService _localStorage;
 
-        public OrderService(HttpClient http, NavigationManager navigationManager)
+        public OrderService(HttpClient http, NavigationManager navigationManager, ILocalStorageService localStorage)
         {
             _http = http;
             _navigationManager = navigationManager;
+            _localStorage = localStorage;
         }
         public List<Order> Orders { get; set; } = new List<Order>();
 
@@ -19,7 +21,8 @@ namespace TM_Eshop_v1.Client.Services.OrderService
             var result = await _http.PostAsJsonAsync("api/order/createorder", order);
             var responce = await result.Content.ReadFromJsonAsync<List<Order>>();
             Orders = responce;
-            _navigationManager.NavigateTo("");
+            await _localStorage.ClearAsync();
+            _navigationManager.NavigateTo("", forceLoad: true);
         }
 
         public async Task GetOrder()
