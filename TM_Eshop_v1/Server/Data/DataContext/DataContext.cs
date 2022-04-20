23e971354d23ccf03df22a66de609478e1d934d5
Marek Tyl <tylmarek@outlook.com>
2022-04-21 01:07:27 +0200

    code clean up

--- TM_Eshop_v1/Server/Data/DataContext/DataContext.cs
+++ TM_Eshop_v1/Server/Data/DataContext/DataContext.cs
@@ -5,7 +5,6 @@
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Product>? Products { get; set; }
         public DbSet<Category>? Categories { get; set; }
-        public DbSet<Buyer>? Buyers { get; set; }
         public DbSet<Order>? Orders { get; set; }
     }
 }
